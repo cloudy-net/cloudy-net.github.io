d63a908e9acb8146caeebf54545e008779ba0c5b
Björn Göransson <bjorn.a.goransson@gmail.com>
2022-10-02 10:20:16 +0200

    Initial commit with Vite/Preact

---
+++ src/app.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'preact/hooks'
+import './app.css'
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://preactjs.com" target="_blank">
+          <img src="/images/logo.svg" class="logo preact" alt="Preact logo" />
+        </a>
+      </div>
+      <h1>Vite + Preact</h1>
+      <div class="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/app.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p class="read-the-docs">
+        Click on the Vite and Preact logos to learn more
+      </p>
+    </>
+  )
+}
