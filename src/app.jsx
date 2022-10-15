0584c2010752180dc300e1ec8b802e90faaf108f
Björn Göransson <bjorn.a.goransson@gmail.com>
2022-10-15 22:43:57 +0200

    Update copy, add image

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,5 @@
 import Header from './header';
+import SelectImage from '../images/select.png';
 
 export function App() {
   return (
@@ -7,9 +8,9 @@ export function App() {
       <div class="container">
         <section class="hero">
           <hero-cloud>☁</hero-cloud>
-          <h1 class="heading">We don't make the CMS - you do.</h1>
+          <h1 class="heading">This CMS is made for Developers.</h1>
           <div class="intro">
-            Create CRUD applications using EF Core as truth
+            Gain control over the editing experience, without mucking around in hundreds of CRUD files, or getting fleeced by the incumbents.
           </div>
         </section>
         <sepa-rator></sepa-rator>
@@ -18,16 +19,25 @@ export function App() {
         <section>
           <ul class="usp-list">
             <li class="usp-item">
-              <h3 class="usp-heading">EF Core is the data model</h3>
-              <div class="usp-text">
-                Your data model is stored in your EF Core context - Not in the database.
-              </div>
+              <h3 class="usp-heading">Data model in POCOs</h3>
+                <p>Your data model is stored declaratively, in code, version controlled, and deployed with your application.</p>
+                <p>Content types do not get saved in some database, and they do not get created by point and click.</p>
+                <pre style="background: rgba(0, 0, 0, 0.1); padding: 10px;">
+                  public class Book : <span class="opacity-50">INameable</span><br/>
+                  &#123;<br/>
+                  &nbsp;&nbsp;public Guid Id &#123; get; set; &#125;<br/>
+                  &nbsp;&nbsp;public string Name &#123; get; set; &#125;<br/>
+                  &nbsp;&nbsp;<span class="opacity-50">[Select(typeof(Author))]</span><br/>
+                  &nbsp;&nbsp;public Guid AuthorId &#123; get; set; &#125;<br/>
+                  &#125;
+                </pre>
             </li>
             <li class="usp-item">
               <h3 class="usp-heading">Extensible, automatic scaffolding</h3>
-              <div class="usp-text">
-                Using regular Razor partials, UI Hints and <code>[CustomAttributes]</code>, you extend the scaffolding that the CMS toolkit provides you.
-              </div>
+              <p>
+                Using regular Razor partials and UI Hints, you extend the scaffolding that the CMS provides you.
+              </p>
+              <img src={SelectImage} style="max-width: 100%;" alt="Demonstration of how the CMS will scaffold the Select attribute on the POCO. It will give you a preview of the referenced entity, and a dropdown with filtering and pagination capabilities." />
             </li>
           </ul>
         </section>
